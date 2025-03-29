d912e4a11448113af8339fd9ee2e8e04edaf5010
Oxoniumd <salamiwahley@gmail.com>
2025-03-29 13:34:55 -0700

    dom add event

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,14 @@
-let msg = document.getElementById("message");
+// let msg = document.getElementById("message");
 
-let btn = document.getElementsByName("language");
-let san = document.querySelector(".san");
-console.log(msg);
-console.log(btn);
-console.log(san);
+// let btn = document.getElementsByName("language");
+// let san = document.querySelector(".san");
+// console.log(msg);
+// console.log(btn);
+// console.log(san);
+
+// let txt = document.querySelector(".text");
+// console.log(txt.parentNode);
+
+let second = document.querySelector(".second");
+
+console.log(second.nextSibling);
