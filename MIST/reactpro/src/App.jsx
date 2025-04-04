71ac3d98f0b6b5dcad2f1202489dc80e34a8b3b6
Oxoniumd <salamiwahley@gmail.com>
2025-04-04 01:18:22 -0700

    flx

---
+++ MIST/reactpro/src/App.jsx
@@ -0,0 +1,11 @@
+import Fruits from "./Component/Fruits";
+
+function App() {
+  return (
+    <div className="App">
+      <Fruits />
+    </div>
+  );
+}
+
+export default App;
---
+++ MIST/reactpro/src/Component/Fruits.jsx
@@ -0,0 +1,11 @@
+export default function Fruits() {
+  const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+
+  return (
+    <div>
+      {fruits.map((fruit) => (
+        <li>{fruit}</li>
+      ))}
+    </div>
+  );
+}
